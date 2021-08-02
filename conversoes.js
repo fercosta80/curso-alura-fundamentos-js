bd0a6faac588a04b112e0608bb186fca76c7aa85
Fernando Lima Costa <fernando@localhost.localdomain>
2021-08-02 15:08:36 -0300

    Exemplos Aula 2

---
+++ conversoes.js
@@ -0,0 +1,20 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+// const numero = 456;
+// const numeroString = '456';
+
+// console.log(numero + numeroString);
+
+
+
+
+// conversão explícita
+//Numer()
+//String()
+
+const numero = 456;
+const numeroString = Number('456a')
+
+console.log(numero + numeroString)
