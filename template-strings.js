50ff1e190fe1282667484f4a905ac05eadf2c65b
Fernando Lima Costa <fernando@localhost.localdomain>
2021-08-04 15:24:33 -0300

    Aula4

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Fernando"
+const idade = 2021-1977
+const cidadeNascimento = 'Jataí'
+//const apresentação = 'meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de "+ cidade
+const apresentação = `meu nome é ${nome}. Minha idade é ${idade}
+e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentação)
+
