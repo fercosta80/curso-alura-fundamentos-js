0165a438cce180e2c13ea231045082ca253aa56f
Fernando Lima Costa <fernando@localhost.localdomain>
2021-08-04 14:51:15 -0300

    Aula 3

---
+++ comparacoes.js
@@ -0,0 +1,11 @@
+// == (comparação implícita - compara só o valor)
+
+const numero = 5;
+const texto = '5';
+
+console.log(numero === texto)
+console.log(typeof numero)
+console.log(typeof texto)
+
+// === comparação explícita  - compara o valor e o tipo de dados)
+// boas práticas recomendam a utilização dos três iguais.
