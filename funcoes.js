9e624f320294111fad102730277c9d2c2ffd7a70
Fernando Lima Costa <fernando@localhost.localdomain>
2021-08-05 14:55:44 -0300

    Funçoes

--- funcoes.js
+++ funcoes.js
@@ -1,2 +1,16 @@
-let x = '';
-console.log
+//let x = '';
+//console.log(x)
+//x = 'oi'
+
+
+// 1) declaração da função
+
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + mais vezes)
+imprimeTexto('Fernando')
+imprimeTexto('texto1')
+
+
